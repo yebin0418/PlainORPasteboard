c1675a3832a7591ebbc91dd1ac729b04cb49f1c0
yebinpark <yb507970@dal.ca>
2020-12-03 20:47:44 -0400

    added comments

--- SceneDelegate.swift
+++ SceneDelegate.swift
@@ -30,6 +30,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        
+        //It allows paste from the Pasteboard every time it becomes active
         if let vc = window?.rootViewController as? ViewController {
             vc.addText()
         }
--- ViewController.swift
+++ ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var textView: UITextView!
     
+    //creating string array
     var pastedStrings: [String] = []
     
     override func viewDidLoad() {
@@ -24,15 +25,21 @@ class ViewController: UIViewController {
         showText()
     }
     
+    //creating addText
     func addText() {
+        
+        /*NOTE: guard allows to stop executing code in this method if a certain condition is not met.
+          It return when text is already inside of the array. If not, it will add into the array */
         guard let text = UIPasteboard.general.string, !pastedStrings.contains(text) else {
             return
         }
         pastedStrings.append(text)
+        //The data will be saved to persistent storage
         UserDefaults.standard.set(pastedStrings, forKey: DATA_KEY)
         showText()
     }
     
+    //The showText method dispaly any copied strings from pastedStrings array
     func showText() {
         textView.text = ""
         for str in pastedStrings {
@@ -40,6 +47,7 @@ class ViewController: UIViewController {
         }
     }
     
+    //This method will remove all of the saved data OR there will be only the single last thing that copied 
     @IBAction func trashWasPressed(_ sender: Any) {
         pastedStrings.removeAll()
         textView.text = ""
